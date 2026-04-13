bf06a5dc84bb06cf255bf5302a4667063607ef5e
Pannala Govardhan <pannalagovardhanreddy880@gmail.com>
2026-04-13 07:28:50 +0530

    fixed port for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.get("/", (req, res) => {
   res.send("Scam Detection API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
